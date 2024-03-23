184355a1e0d30d930013e2a15b1b174e1b0a079b
Nikolay Chunosov <N.Chunosov@yandex.ru>
2024-03-23 12:18:08 +0700

    add demo code for beam image rendering

---
+++ calc/render_1.cmd
@@ -0,0 +1,3 @@
+:: Math optimization flags are important here.
+:: taken from http://spfrnd.de/posts/2018-03-10-fast-exponential.html
+c++ -O3 -ffast-math -funsafe-math-optimizations -msse4.2 -o render_1 render_1.c && render_1
