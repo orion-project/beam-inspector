fbfb77cdf87a03129c013975ea36816384df3b20
Nikolay Chunosov <N.Chunosov@yandex.ru>
2024-04-21 16:32:44 +0700

    use PopupMessage for measurement messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "app/HelpSystem.h"
 #include "windows/PlotWindow.h"
 
+#include "helpers/OriTheme.h"
+
 #include <QApplication>
+
+#ifndef Q_OS_WIN
+#include <iostream>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -15,6 +21,10 @@ int main(int argc, char *argv[])
     app.setOrganizationName("orion-project.org");
     app.setApplicationVersion(HelpSystem::appVersion());
     app.setStyle("Fusion");
+
+    // Call `setStyleSheet` after setting loaded
+    // to be able to apply custom colors.
+    app.setStyleSheet(Ori::Theme::makeStyleSheet(Ori::Theme::loadRawStyleSheet()));
 
     PlotWindow w;
     w.show();
