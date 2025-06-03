7121acedbbef62c860e9b8b5a65fc5178f9b2a07
Chunosov <N.Chunosov@yandex.ru>
2025-06-03 22:10:22 +0700

    optimize stab graph scrolling

--- src/widgets/StabilityView.cpp
+++ src/widgets/StabilityView.cpp
@@ -2,17 +2,20 @@
 
 #include "helpers/OriLayouts.h"
 #include "helpers/OriWidgets.h"
+#include "widgets/OriPopupMessage.h"
 
 #include "qcustomplot/src/core.h"
 #include "qcustomplot/src/axis/axistickerdatetime.h"
 #include "qcustomplot/src/layoutelements/layoutelement-axisrect.h"
 #include "qcustomplot/src/layoutelements/layoutelement-legend.h"
+#include "qcustomplot/src/layoutelements/layoutelement-textelement.h"
 #include "qcustomplot/src/plottables/plottable-graph.h"
 
 #define A_ Ori::Gui::action
 
 #define UPDATE_INTERVAL_MS 1000
-#define DISPLAY_RANGE_S 600
+#define CLEAN_INTERVAL_MS 60000
+#define DISPLAY_RANGE_S 300
 #define DATA_BUF_SIZE 10
 
 StabilityView::StabilityView(QWidget *parent) : QWidget{parent}
@@ -43,10 +46,16 @@ StabilityView::StabilityView(QWidget *parent) : QWidget{parent}
     _timelineX->setName("Center X");
     _timelineY->setName("Center Y");
     
+    _timelineLen = new QCPTextElement(_plotTime);
+    
+    _plotTime->axisRect()->insetLayout()->addElement(_timelineLen, Qt::AlignRight|Qt::AlignTop);
+    
     setThemeColors(PlotHelpers::SYSTEM, false);
     
-    _plotTime->addAction(A_(tr("Copy X Points"), this, [this]{ PlotHelpers::copyGraph(_timelineX); }));
-    _plotTime->addAction(A_(tr("Copy Y Points"), this, [this]{ PlotHelpers::copyGraph(_timelineY); }));
+    _plotTime->addAction(A_(tr("Reset"), this, &StabilityView::cleanResult, ":/toolbar/update"));
+    _plotTime->addAction(Ori::Gui::separatorAction(this));
+    _plotTime->addAction(A_(tr("Copy X Points"), this, [this]{ copyGraph(_timelineX); }));
+    _plotTime->addAction(A_(tr("Copy Y Points"), this, [this]{ copyGraph(_timelineY); }));
     
     _plotTime->addAction(A_(tr("Copy as Image"), this, [this]{
         PlotHelpers::copyImage(_plotTime, [this](PlotHelpers::Theme theme){ setThemeColors(theme, false); });
@@ -63,8 +72,9 @@ void StabilityView::setThemeColors(PlotHelpers::Theme theme, bool replot)
 {
     PlotHelpers::setThemeColors(_plotTime, theme);
     PlotHelpers::setThemeColors(_plotHeat, theme);
-    _timelineX->setPen(QPen(QColor(0, 0, 255, 150), 2));
-    _timelineY->setPen(QPen(QColor(255, 0, 0, 150), 2));
+    _timelineLen->setTextColor(PlotHelpers::themeAxisLabelColor(theme));
+    _timelineX->setPen(QPen(QColor(0, 0, 225, 125)));
+    _timelineY->setPen(QPen(QColor(255, 0, 0, 125)));
     if (replot) {
         _plotTime->replot();
         _plotHeat->replot();
@@ -80,8 +90,7 @@ void StabilityView::setConfig(const PixelScale& scale)
     _scale = scale;
 
     QTimer::singleShot(1000, this, [this]{
-        _timelineMinV = qQNaN();
-        _timelineMaxV = qQNaN();
+        resetScale(false, true);
     });
 }
 
@@ -95,21 +104,25 @@ void StabilityView::setResult(qint64 time, const QList<CgnBeamResult>& val)
     if (r.nan)
         return;
         
-    _frameTime = time;
+    if (_dataBufCursor >= DATA_BUF_SIZE)
+        return;
+        
     _dataBuf[_dataBufCursor] = DataPoint {
         .time = time,
         .x = r.xc,
         .y = r.yc,
     };
-    if (_dataBufCursor < DATA_BUF_SIZE-1)
-        _dataBufCursor++;
+    _dataBufCursor++;
+    _frameTime = time;
+    if (_startTime < 0)
+        _startTime = _frameTime;
 }
 
 void StabilityView::showResult()
 {
     if (!(_showTime < 0 || _frameTime - _showTime >= UPDATE_INTERVAL_MS))
         return;
-
+        
     int newPoints = _dataBufCursor;
     bool timelineRangeChangedV = false;
     for (int i = 0; i < newPoints; i++) {
@@ -143,18 +156,32 @@ void StabilityView::showResult()
         _plotTime->yAxis->setRange(_timelineMinV - m, _timelineMaxV + m);
     }
     
-    if (_timelineMaxK - _timelineMinK > DISPLAY_RANGE_S) {
-        _timelineMinK = _timelineMaxK - DISPLAY_RANGE_S;
-        _timelineX->data()->removeBefore(_timelineMinK);
-        _timelineY->data()->removeBefore(_timelineMinK);
-    }
-    _plotTime->xAxis->setRange(_timelineMinK, _timelineMaxK);
+    if (_cleanTime < 0) {
+        _cleanTime = _frameTime;
+    } else if (_frameTime - _cleanTime >= CLEAN_INTERVAL_MS) {
+        if (_timelineMaxK - _timelineMinK > DISPLAY_RANGE_S) {
+            _timelineMinK = _timelineMaxK - DISPLAY_RANGE_S;
+            _timelineX->data()->removeBefore(_timelineMinK);
+            _timelineY->data()->removeBefore(_timelineMinK);
+            _cleanTime = _frameTime;
+        }
+    } 
+    _plotTime->xAxis->setRange(timelineDisplayMinK(), _timelineMaxK);
 
     _plotTime->replot();
     _plotHeat->replot();
     
+    _timelineLen->setText(formatSecs((_frameTime - _startTime) / 1000));
+    
+    //qDebug() << _timelineX->dataCount() << _plotTime->replotTime(true);
+    
     _showTime = _frameTime;
     _dataBufCursor = 0;
+}
+
+double StabilityView::timelineDisplayMinK() const
+{
+    return qMax(_timelineMinK, _timelineMaxK-DISPLAY_RANGE_S);
 }
 
 void StabilityView::cleanResult()
@@ -163,10 +190,38 @@ void StabilityView::cleanResult()
     _timelineY->data().clear();
     _frameTime = -1;
     _showTime = -1;
-    _timelineMinK = -1;
-    _timelineMaxK = -1;
-    _timelineMinV = qQNaN();
-    _timelineMaxV = qQNaN();
+    _cleanTime = -1;
+    _startTime = -1;
     _offsetX = -1;
     _offsetY = -1;
+    _dataBufCursor = 0;
+    _timelineLen->setText(QString());
+    resetScale(true, true);
+}
+
+void StabilityView::resetScale(bool key, bool value)
+{
+    if (key) {
+        _timelineMinK = -1;
+        _timelineMaxK = -1;
+    }
+    if (value) {
+        _timelineMinV = qQNaN();
+        _timelineMaxV = qQNaN();
+    }
+}
+
+void StabilityView::copyGraph(QCPGraph *graph)
+{
+    QString res;
+    QTextStream s(&res);
+    double startKey = timelineDisplayMinK();
+    foreach (const auto& p, graph->data()->rawData()) {
+        if (p.key < startKey)
+            continue;
+        QString timeStr = QDateTime::fromSecsSinceEpoch(p.key).toString(Qt::ISODateWithMs);
+        s << timeStr << ',' << QString::number(p.value) << '\n';
+    }
+    qApp->clipboard()->setText(res);
+    Ori::Gui::PopupMessage::affirm(qApp->tr("Data points been copied to Clipboard"), Qt::AlignRight|Qt::AlignBottom);
 }
--- src/widgets/StabilityView.h
+++ src/widgets/StabilityView.h
@@ -9,6 +9,7 @@
 #include <QWidget>
 
 class QCPGraph;
+class QCPTextElement;
 class QCustomPlot;
 
 class StabilityView : public QWidget
@@ -34,13 +35,18 @@ private:
 
     QCustomPlot *_plotTime, *_plotHeat;
     QCPGraph *_timelineX, *_timelineY;
+    QCPTextElement *_timelineLen;
     PixelScale _scale;
     double _timelineMinK = -1, _timelineMaxK = -1;
     double _timelineMinV = -1, _timelineMaxV = -1;
     double _offsetX = -1, _offsetY = -1;
-    qint64 _frameTime = -1, _showTime = -1;
+    qint64 _frameTime = -1, _showTime = -1, _cleanTime = -1, _startTime = -1;
     QVector<DataPoint> _dataBuf;
     int _dataBufCursor = 0;
+    
+    void resetScale(bool key, bool value);
+    double timelineDisplayMinK() const;
+    void copyGraph(QCPGraph *graph);
 };
 
 #endif // STABILITY_VIEW_H
